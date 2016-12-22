7973d2555158021840a1ef6a8b25ce95e13a02a5
dharanivani <dharani.free@gmail.com>
2016-12-22 07:10:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Application</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="css/search.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<h2>Search Box</h2>
+           <div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="  search-query form-control" placeholder="Search" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+	</div>
+</div>
+</body>
